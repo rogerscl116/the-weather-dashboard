4086571698ea706990bb575d7e67812244eaff47
charity.rogers <rogerscl116@gmail.com>
2021-05-29 16:31:45 -0500

    add formSubmitHandler function and event listener for search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,60 @@
 var searchButton = document.querySelector("#search-button");
 var clearButton = document.querySelector("#clear-button");
 var inputEl = document.querySelector("#city");
-var apiKey = "259fafdbfd373c06e5bf522bb9ac44cc"
+var currentWeatherDiv = document.querySelector("#current-weather-container");
+var fiveDayDiv = document.querySelector("#five-day-container");
+
+var formSubmitHandler = function(event) {
+
+// prevent page from refreshing
+event.preventDefault();
+
+// get value from input element
+var cityInput = inputEl.value.trim();
+
+if (cityInput) {
+getWeather(cityInput);
+
+
+// clear old content
+currentWeatherDiv = "";
+fiveDayDiv = "";
+inputEl.value = "";
+} else {
+alert("Please enter a city.");
+}
+};
 
 
 // create function when search button is clicked
-// function getWeather()
-var inputValue = inputEl.value;
-
 // fetch current weather from api
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}`)
-.then(function(response) {
-    response.json()
-    .then(function(data) {
+var getWeather = function(city) {
+
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=259fafdbfd373c06e5bf522bb9ac44cc"
+
+ fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          displayWeather(data, city);
+        });
+      } else {
+        alert('Error: City Not Found');
+      }
     })
-});
-
-
-// locally store cities searched
-
-
-
-// add search button event listener for new city
-// searchButton.addEventListener("submit", 
-
-
-// add button event listeners for saved cities
+    .catch(function(error) {
+      alert("Unable to connect to OpenWeather");
+    });
+};
 
 
 
-//clear searched cities from localstorage
+// add searched cities to localstorage
+// clear searched cities from localstorage
+
+
+// add event listeners for search button and saved cities
+searchButton.addEventListener("submit", formSubmitHandler);
